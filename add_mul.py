a41450abe002ceae9b5a3eea6aaafe7cf8545088
김스시 <57784879+omakasekim@users.noreply.github.com>
2021-11-27 12:47:32 +0900

    Create add_mul.py

---
+++ add_mul.py
@@ -0,0 +1,14 @@
+def add_mul(choice, *args):
+    if choice == "add":
+        result = 0
+        for i in args:
+            result = result + i
+    elif choice == "mul":
+        result = 1
+        for i in args:
+            result = result * i
+    return result
+
+
+result = add_mul('add', 1, 2, 3, 4)
+print(result)
