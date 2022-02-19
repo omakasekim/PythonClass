bf61b70be7f1a08930c0b9924017a11bedf784ec
김스시 <57784879+omakasekim@users.noreply.github.com>
2022-02-19 11:21:09 +0900

    Create pltpractice.py

---
+++ 220219/pltpractice.py
@@ -0,0 +1,12 @@
+
+plt.plot(df['학력'],df['평균(분)'],'g--', label='학력 별 여가시간')
+plt.plot(df['학력'],df['평균(분)']+100,'b-.',label='학력 별 여가시간 + 100')
+plt.xlabel('학력')
+plt.ylabel('여가 시간')
+plt.legend(loc='best')
+
+
+plt.rcParams['axes.unicode_minus']=False
+plt.rcParams['font.family']='Malgun Gothic'
+plt.rcParams['font.size']= 7
+plt.show()
